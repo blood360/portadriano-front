90b84fec95b720dac21bf3c45117da25e8fae6ec
Adriano Santos <adrianodev2025@outlook.com>
2025-08-14 11:28:48 -0300

    corrigindo db dnv

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,17 +4,17 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL, // Agora o código está seguro e flexível!
-  {
-    dialect: 'postgres',
-    logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
-  }
+  process.env.DATABASE_URL, // Deixa só a variável aqui!
+  {
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
 );
 
 module.exports = sequelize;
